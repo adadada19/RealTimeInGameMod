91221b71a6d29a5b2626aea9b8050d600ed21408
another one <adadada1930@gmail.com>
2022-03-15 19:49:56 +0300

    Added gun sprite reworked it's behavior

--- Items/BoneShooter.cs
+++ Items/BoneShooter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ID;
@@ -23,39 +24,55 @@ namespace RealTimeInGameMod.Items
         {
             item.damage = 30;
             item.ranged = true;
-            item.width = 20;
-            item.height = 20;
+            item.width = 64;
+            item.height = 16;
             item.useTime = 10;
             item.useAnimation = 10;
             item.useStyle = ItemUseStyleID.HoldingOut;
             item.noMelee = true;
             item.knockBack = 0f;
             item.rare = ItemRarityID.Expert;
-            item.UseSound = SoundID.Item11;
+            item.UseSound = SoundID.Item38;
             item.autoReuse = true;
             item.shoot = ModContent.ProjectileType<BoneProjectile>();
             item.shootSpeed = 16f;
             item.useAmmo = ItemID.Bone;
+            item.scale = 0.8f;
         }
         public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
         {
-/*            if (player.position.X > Main.MouseWorld.X)
+            /*            if (player.position.X > Main.MouseWorld.X)
+                        {
+                            position += new Vector2(-50, -12);
+                        }
+                        else if (player.position.X < Main.MouseWorld.X)
+                        {
+                            position += new Vector2(50, -12);
+                        }
+                        else
+                        {*/
+            // position += new Vector2(0, -5);
+
+            /*            }*/
+            Vector2 muzzleOffset = Vector2.Normalize(new Vector2(speedX, speedY)) * 60f;
+            if (Collision.CanHit(position, 0, 0, position + muzzleOffset, 0, 0))
             {
-                position += new Vector2(-50, -12);
+                position += muzzleOffset;
             }
-            else if (player.position.X < Main.MouseWorld.X)
+            int numberProjectiles = 4 + Main.rand.Next(2); // 4 or 5 shots
+            for (int i = 0; i < numberProjectiles; i++)
             {
-                position += new Vector2(50, -12);
+                Vector2 perturbedSpeed = new Vector2(speedX, speedY).RotatedByRandom(MathHelper.ToRadians(30)); // 30 degree spread.
+                                                                                                                // If you want to randomize the speed to stagger the projectiles
+                                                                                                                // float scale = 1f - (Main.rand.NextFloat() * .3f);
+                                                                                                                // perturbedSpeed = perturbedSpeed * scale; 
+                Projectile.NewProjectile(position.X, position.Y, perturbedSpeed.X, perturbedSpeed.Y, type, damage, knockBack, player.whoAmI);
             }
-            else
-            {*/
-                position += new Vector2(0, -12);
-/*            }*/
-            return true;
+            return false;
         }
-/*        public override Vector2? HoldoutOffset()
+        public override Vector2? HoldoutOffset()
         {
-            return new Vector2(0, 14);
-        }*/
+            return new Vector2(-30, 3);
+        }
     }
 }
