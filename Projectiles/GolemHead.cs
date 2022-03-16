ddf907fbc1931481e07d923cf364f4c516be80b7
another one <adadada1930@gmail.com>
2022-03-16 19:18:52 +0300

    Added AI t golem's head. TODO - prjectiles from his eyes

--- Projectiles/GolemHead.cs
+++ Projectiles/GolemHead.cs
@@ -46,7 +46,115 @@ namespace RealTimeInGameMod.Projectiles
             idlePosition.Y -= 48f;
             float minionPositionOffsetX = (10 + projectile.minionPos * 40) * -player.direction;
             idlePosition.X += minionPositionOffsetX;
+            Vector2 vectorToIdlePosition = idlePosition - projectile.Center;
+            float distanceToIdlePosition = vectorToIdlePosition.Length();
+            if (Main.myPlayer == player.whoAmI && distanceToIdlePosition > 2000f)
+            {
+                // Whenever you deal with non-regular events that change the behavior or position drastically, make sure to only run the code on the owner of the projectile,
+                // and then set netUpdate to true
+                projectile.position = idlePosition;
+                projectile.velocity *= 0.1f;
+                projectile.netUpdate = true;
+            }
+            float overlapVelocity = 0.04f;
+            for (int i = 0; i < Main.maxProjectiles; i++)
+            {
+                // Fix overlap with other minions
+                Projectile other = Main.projectile[i];
+                if (i != projectile.whoAmI && other.active && other.owner == projectile.owner && Math.Abs(projectile.position.X - other.position.X) + Math.Abs(projectile.position.Y - other.position.Y) < projectile.width)
+                {
+                    if (projectile.position.X < other.position.X) projectile.velocity.X -= overlapVelocity;
+                    else projectile.velocity.X += overlapVelocity;
 
+                    if (projectile.position.Y < other.position.Y) projectile.velocity.Y -= overlapVelocity;
+                    else projectile.velocity.Y += overlapVelocity;
+                }
+            }
+            float speed = 8f;
+            float inertia = 20f;
+            if (distanceToIdlePosition > 600f)
+            {
+                // Speed up the minion if it's away from the player
+                speed = 12f;
+                inertia = 60f;
+            }
+            else
+            {
+                // Slow down the minion if closer to the player
+                speed = 4f;
+                inertia = 80f;
+            }
+            if (distanceToIdlePosition > 20f)
+            {
+                // The immediate range around the player (when it passively floats about)
+
+                // This is a simple movement formula using the two parameters and its desired direction to create a "homing" movement
+                vectorToIdlePosition.Normalize();
+                vectorToIdlePosition *= speed;
+                projectile.velocity = (projectile.velocity * (inertia - 1) + vectorToIdlePosition) / inertia;
+            }
+            /*Vector2 idlePosition = new Vector2(player.position.X, player.position.Y);
+            if (idlePosition.X > projectile.position.X)
+            {
+                Vector2 BackOfHead1 = new Vector2(player.Center.X - 40f, player.Center.Y - 20f);
+                Vector2 MoveToHead1 = new Vector2(BackOfHead1.X - projectile.Center.X, BackOfHead1.Y - projectile.Center.Y);
+                
+                if (projectile.Center != BackOfHead1)
+                {
+                    MoveToHead1.Normalize();
+                    projectile.velocity += MoveToHead1/10f;
+                }
+                else
+                    projectile.velocity = Vector2.Zero;
+            }
+            else if (idlePosition.X < projectile.position.X)
+            {
+                Vector2 BackOfHead1 = new Vector2(player.Center.X + 20f, player.Center.Y - 20f);
+                Vector2 MoveToHead1 = new Vector2(BackOfHead1.X - projectile.Center.X, BackOfHead1.Y - projectile.Center.Y);
+                
+                if (projectile.Center != BackOfHead1)
+                {
+                    MoveToHead1.Normalize();
+                    projectile.velocity += MoveToHead1/10f;
+                }
+                else
+                    projectile.velocity = Vector2.Zero;
+            }*/
+            int b = -1;
+            var pos = projectile.position;
+            Vector2 targetCenter = pos;
+            // the max distance
+            float distanceFromTarget = 700f;
+            // loop over npc
+            for (int i = 0; i < Main.maxNPCs; i++)
+            {
+                // define npc
+                NPC npc = Main.npc[i];
+                // check if npc can be chased
+                if (npc.CanBeChasedBy())
+                {
+                    float between = Vector2.Distance(npc.Center, pos);
+                    bool closest = Vector2.Distance(pos, targetCenter) > between;
+                    bool inRange = between < distanceFromTarget;
+                    // check if the npc is the closest npc or no target has been found
+                    if ((closest && inRange) || b == -1)
+                    {
+                        //set targetcenter and b to the index
+                        b = i;
+                        targetCenter = npc.Center;
+                    }
+                }
+            }
+            // check if the index not -1
+            if (b != -1)
+            {
+                //the shoot speed
+                float insertsomefloathere = 10f;
+                // use DirectionTo
+                Vector2 velToEnemy = projectile.DirectionTo(targetCenter) * insertsomefloathere;
+                //call proj.newproj from using the velocity to enemy
+                Projectile.NewProjectile(projectile.Center, velToEnemy, /*here spawn laser*/);
+            }
         }
-    }
+        }
 }
